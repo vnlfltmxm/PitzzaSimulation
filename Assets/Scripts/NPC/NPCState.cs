50410387e31c0ec12b9c7c8a4d447a6036390114
박상욱 <vnlfltmxm@gmail.com>
2024-06-26 23:00:16 +0900

    [Add] NPC 상태패턴에 따른 클래스 추가

---
+++ Assets/Scripts/NPC/NPCState.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCIdleState : BaseState<NPCController>
+{
+    public NPCIdleState(NPCController npcController) : base(npcController)
+    {
+
+    }
+
+    public override void OnEnterState()
+    {
+    }
+
+    public override void OnExitState()
+    {
+    }
+
+    public override void OnUpdateState()
+    {
+    }
+}
+
+public class NPCWalkState : BaseState<NPCController>
+{
+    public NPCWalkState(NPCController npcController) : base(npcController)
+    {
+
+    }
+
+    public override void OnEnterState()
+    {
+    }
+
+    public override void OnExitState()
+    {
+    }
+
+    public override void OnUpdateState()
+    {
+    }
+}
+
+public class NPCOrderState : BaseState<NPCController>
+{
+    public NPCOrderState(NPCController npcController) : base(npcController)
+    {
+
+    }
+
+    public override void OnEnterState()
+    {
+    }
+
+    public override void OnExitState()
+    {
+    }
+
+    public override void OnUpdateState()
+    {
+    }
+}
+
+public class NPCLeaveState : BaseState<NPCController>
+{
+    public NPCLeaveState(NPCController npcController) : base(npcController)
+    {
+
+    }
+
+    public override void OnEnterState()
+    {
+    }
+
+    public override void OnExitState()
+    {
+    }
+
+    public override void OnUpdateState()
+    {
+    }
+}
