8b757fa5ad0862f6c6c6e31875e169776511e6d7
박상욱 <vnlfltmxm@gmail.com>
2024-07-11 10:07:36 +0900

    [fix] 빌드문제 해결

--- Assets/Scripts/Manger/DataManger.cs
+++ Assets/Scripts/Manger/DataManger.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Linq;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 using System.IO;
