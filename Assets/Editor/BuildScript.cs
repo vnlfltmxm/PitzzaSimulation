5f8aabe61d7a3ae06e5741e1f1798ee326103cbe
박상욱 <vnlfltmxm@gmail.com>
2025-01-01 14:12:14 +0900

    [fix] 폴더가 없을시 폴더생성

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -6,7 +6,13 @@ public class BuildScript
 {
     public static void Build() 
     {
-        string outputPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Pizza.exe");
+        string outputPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Pizza.exe");//바탕화면에 생성
+        string directory = Path.GetDirectoryName(outputPath);
+
+        if (!Directory.Exists(directory))//폴더가 없다면 폴더생성
+        {
+            Directory.CreateDirectory(directory);
+        }
         BuildPipeline.BuildPlayer(new[] { "Assets/Scenes/PlayScene.unity" }, outputPath, BuildTarget.StandaloneWindows64, BuildOptions.None);
     }
 
